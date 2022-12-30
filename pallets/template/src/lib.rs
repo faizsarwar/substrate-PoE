528b8121f9a4f8bd5542d580011f904a9ce043ea
faiz <faizsarwar856@gmail.com>
2022-12-31 01:51:50 +0500

    added code

---
+++ pallets/template/src/lib.rs
@@ -0,0 +1,101 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+/// Edit this file to define custom logic or remove it if it is not needed.
+/// Learn more about FRAME and the core library of Substrate FRAME pallets:
+/// https://substrate.dev/docs/en/knowledgebase/runtime/frame
+
+use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, traits::Get};
+use frame_system::ensure_signed;
+use sp_std::vec::Vec;
+use frame_support::ensure;
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod tests;
+
+pub trait Config: frame_system::Config {
+	/// Because this pallet emits events, it depends on the runtime's definition of an event.
+	type Event: From<Event<Self>> + Into<<Self as frame_system::Config>::Event>;
+}
+
+decl_storage! {
+	trait Store for Module<T: Config> as TemplateModule {
+
+
+        Proofs: map hasher(blake2_128_concat) Vec<u8> => (T::AccountId, T::BlockNumber);
+	}
+}
+
+decl_event!(
+	pub enum Event<T> where AccountId = <T as frame_system::Config>::AccountId {
+		/// Event emitted when a proof has been claimed. [who, claim]
+		ClaimCreated(AccountId, Vec<u8>),
+		/// Event emitted when a claim is revoked by the owner. [who, claim]
+		ClaimRevoked(AccountId, Vec<u8>),
+	}
+);
+
+// Errors inform users that something went wrong.
+decl_error! {
+	pub enum Error for Module<T: Config> {
+		/// Error names should be descriptive.
+		NoneValue,
+        /// The proof has already been claimed.
+        ProofAlreadyClaimed,
+        /// The proof does not exist, so it cannot be revoked.
+        NoSuchProof,
+        /// The proof is claimed by another account, so caller can't revoke it.
+        NotProofOwner,
+	}
+}
+
+decl_module! {
+	pub struct Module<T: Config> for enum Call where origin: T::Origin {
+		// Errors must be initialized if they are used by the pallet.
+		type Error = Error<T>;
+
+		// Events must be initialized if they are used by the pallet.
+		fn deposit_event() = default;
+
+        /// Allow a user to claim ownership of an unclaimed proof.
+        #[weight = 10_000]
+        fn create_claim(origin, proof: Vec<u8>) {
+            let sender = ensure_signed(origin)?;
+
+            // Verify that the specified proof has not already been claimed.
+            ensure!(!Proofs::<T>::contains_key(&proof), Error::<T>::ProofAlreadyClaimed);
+
+            // Get the block number from the FRAME System module.
+            let current_block = <frame_system::Module<T>>::block_number();
+
+            // Store the proof with the sender and block number.
+            Proofs::<T>::insert(&proof, (&sender, current_block));
+
+            // Emit an event that the claim was created.
+            Self::deposit_event(RawEvent::ClaimCreated(sender, proof));
+        }
+
+		/// Allow the owner to revoke their claim.
+		#[weight = 10_000]
+		fn revoke_claim(origin, proof: Vec<u8>) {
+			let sender = ensure_signed(origin)?;
+
+			// Verify that the specified proof has been claimed.
+			ensure!(Proofs::<T>::contains_key(&proof), Error::<T>::NoSuchProof);
+
+			// Get owner of the claim.
+			let (owner, _) = Proofs::<T>::get(&proof);
+
+			// Verify that sender of the current call is the claim owner.
+			ensure!(sender == owner, Error::<T>::NotProofOwner);
+
+			// Remove claim from storage.
+			Proofs::<T>::remove(&proof);
+
+			// Emit an event that the claim was erased.
+			Self::deposit_event(RawEvent::ClaimRevoked(sender, proof));
+		}
+	}
+}
